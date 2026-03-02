c9e4e4a4edeaf5dbf1ea677267c2a1d041e8bf3a
ayman <aemanharde@gmail.log>
2026-03-03 00:38:38 +0300

    baisc UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return <Main />;
+}
+
+function Main() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h2 className="title">Solve Problems</h2>
+        <h3>subtitle</h3>
+      </div>
+      <div className="body">
+        <div className="levels">
+          <p>Choose a Level :</p>
+          <button id="easy">Easy</button>
+          <button id="medium">Medium</button>
+          <button id="hard">Hard</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
