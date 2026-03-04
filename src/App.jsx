1c4f2028d8a6eb883c18a651bf4100ebcdff4042
ayman <aemanharde@gmail.com>
2026-03-04 04:38:35 +0300

    getting levels for each difficulty level

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
+import { useState } from "react";
 import "./App.css";
 
+const easyLevels = [
+  {
+    id: 1,
+    title: "",
+    subTitle: "",
+    case: "",
+    img: "",
+    options: "",
+    solution: "",
+    explanation: "",
+    timeLimit: "",
+  },
+  {
+    id: 1,
+    title: "",
+    subTitle: "",
+    case: "",
+    img: "",
+    options: "",
+    solution: "",
+    explanation: "",
+    timeLimit: "",
+  },
+];
+const mediumLevels = [
+  {
+    id: 1,
+    title: "",
+    subTitle: "",
+    case: "",
+    img: "",
+    options: "",
+    solution: "",
+    explanation: "",
+    timeLimit: "",
+  },
+];
+const hardLevels = [
+  {
+    id: 1,
+    title: "",
+    subTitle: "",
+    case: "",
+    img: "",
+    options: "",
+    solution: "",
+    explanation: "",
+    timeLimit: "",
+  },
+];
 function App() {
   return <Main />;
 }
 
 function Main() {
+  const [difficulty, setDifficulty] = useState("");
+
+  function handleSetDifficulty(level) {
+    setDifficulty(level);
+  }
   return (
     <div className="container">
       <div className="header">
@@ -14,21 +70,18 @@ function Main() {
       <div className="body">
         <div className="difficulty">
           <p>Choose a Difficulty :</p>
-          <button id="easy">Easy</button>
-          <button id="medium">Medium</button>
-          <button id="hard">Hard</button>
+          <button id="easy" onClick={() => handleSetDifficulty(easyLevels)}>
+            Easy
+          </button>
+          <button id="medium" onClick={() => handleSetDifficulty(mediumLevels)}>
+            Medium
+          </button>
+          <button id="hard" onClick={() => handleSetDifficulty(hardLevels)}>
+            Hard
+          </button>
         </div>
       </div>
-    </div>
-  );
-}
-
-function Levels() {
-  return (
-    <div className="levels">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      {difficulty && <Levels difficulty={difficulty} />}
     </div>
   );
 }
